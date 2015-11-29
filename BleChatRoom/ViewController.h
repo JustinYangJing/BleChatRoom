4d115ce713095e7c80214cce0768ccca1ae63980
JustinYang <ityangjing@gmail.com>
2015-11-29 14:07:34 +0800

    Initial Commit

---
+++ BleChatRoom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BleChatRoom
+//
+//  Created by JustinYang on 11/29/15.
+//  Copyright © 2015 JustinYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BleChatRoom/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BleChatRoom
+//
+//  Created by JustinYang on 11/29/15.
+//  Copyright © 2015 JustinYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
